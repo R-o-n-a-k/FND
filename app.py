15f53307187e07a244883d1462f41bf26a2bb412
Ronak <rroonnaakk7@gmail.com>
2024-05-05 12:56:56 +0530

    ref: trying other approach

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
-from flask import url_for
-
 
 app = Flask(__name__)
 load_model = pickle.load(open('NPAfinal_model.sav', 'rb'))
     
-@app.route("/favicon.ico")
-def favicon():
-    return url_for('static', filename='favicon.ico')
-
 @app.route("/")
 def hello():
     return render_template("index.html")
